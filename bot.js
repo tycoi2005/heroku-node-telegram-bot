94f4596a4dbc68728afb3549ffeb78df2e42f9df
toanhd <toanhd@cardano-labo>
2017-09-27 19:58:19 +0700

    Ready to run on heroku

--- bot.js
+++ bot.js
@@ -15,7 +15,21 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '! I am pleased to serve you!').then(function (res) {
+      // { message_id: 12,
+      //     from:
+      //     { id: 463309037,
+      //         is_bot: true,
+      //         first_name: 'coin-monitor',
+      //         username: 'botbiecoin_bot' },
+      //     chat:
+      //     { id: 307822770,
+      //         first_name: 'toan',
+      //         last_name: 'huynh',
+      //         type: 'private' },
+      //     date: 1506516731,
+      //         text: 'Hello, toan!' }
+    // console.log("res",res)
     // reply sent!
   });
 });
