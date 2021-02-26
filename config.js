8e4e363ddc4334fbfef4ea139e07c4d4b2f1ba20
Jaken Ty <toanhdpy@gmail.com>
2021-02-26 18:32:55 +0700

    config 5s

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   timers : {
+    everyFiveSeconds: '*/5 * * * * *',
     everyTenSeconds: '*/10 * * * * *',
     everyTwentySeconds: '*/20 * * * * *',
     everyThirtySeconds: '*/30 * * * * *',
